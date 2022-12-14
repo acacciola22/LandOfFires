e9d68245e4ced7fa4aaf8189931aaf2ea602b7e8
brillantina <rita.marrano.98@gmail.com>
2022-12-14 19:25:44 +0100

    commitD

--- LandOfFires/LandOfFires Shared/GameOverScene.swift
+++ LandOfFires/LandOfFires Shared/GameOverScene.swift
@@ -1,8 +1,8 @@
 //
 //  GameOverScene.swift
-//  LandOfFires iOS
+//  Merdina
 //
-//  Created by antonia on 12/12/22.
+//  Created by Rita Marrano on 09/12/22.
 //
 
 import Foundation
@@ -10,7 +10,8 @@ import SpriteKit
 
 class GameOverScene: SKScene {
     
-    
+    let scores = loadScores()
+    var score : Int = 0
 //    var gameScore : Int = 0
     
     override init(size: CGSize) {
@@ -26,7 +27,25 @@ class GameOverScene: SKScene {
             self.addChild(bg)
         }
         
-        
+        var height = frame.height - 55
+        let width = frame.width/2
+
+
+        let titleNode : SKLabelNode = SKLabelNode(fontNamed: "STV5730A")
+        titleNode.text = "GAME OVER BABY ! !"
+        titleNode.fontSize = 40
+        titleNode.fontColor = .black
+        titleNode.position.y = height
+        height -= 40
+        titleNode.position.x = width
+        addChild(titleNode)
+
+        let personalScoreNode = SKLabelNode(fontNamed: "STV5730A")
+            personalScoreNode.text = "Your current score: \(score)"
+            personalScoreNode.position.y = height
+            personalScoreNode.position.x = width + 20
+            personalScoreNode.fontColor = .black
+            addChild(personalScoreNode)
         
 //        let personalScoreNode = SKLabelNode(fontNamed: "Copperplate-Light")
 //            personalScoreNode.text = "your score: \(gameScore)"
@@ -37,27 +56,25 @@ class GameOverScene: SKScene {
 //
     
     
-        let label = SKLabelNode(fontNamed: "Cochin")
-        label.text = " ViVoglioBene "
-        label.fontSize = 40
-        label.fontColor = .white
-        label.position = CGPoint(x: self.size.width/2 , y: self.size.height/2)
-        self.addChild(label)
+//        let label = SKLabelNode(fontNamed: "2Credits")
+//        label.text = " GAME OVER "
+//        label.fontSize = 50
+//        label.fontColor = .white
+//        label.position = CGPoint(x: self.size.width/2 , y: self.size.height/2)
+//        self.addChild(label)
         
+        
+
         
         let hometButton = SKSpriteNode(imageNamed: "buttonHome")
-
-//        startButton.text = " START "
-//        startButton.fontColor = .black
+        hometButton.setScale(3.5)
         hometButton.position = CGPoint(x: self.size.width/2 , y: 150)
         hometButton.name = " homeButton "
         self.addChild(hometButton)
         
         
         let startButton = SKSpriteNode(imageNamed: "buttonRetry")
-
-//        startButton.text = " START "
-//        startButton.fontColor = .black
+        startButton.setScale(3.5)
         startButton.position = CGPoint(x: self.size.width/2 , y: 50)
         startButton.name = " REPLAY "
         self.addChild(startButton)
---
+++ LandOfFires/LandOfFires Shared/HowToPlayScene.swift
@@ -0,0 +1,65 @@
+//
+//  HowToPlayScene.swift
+//  LandOfFires iOS
+//
+//  Created by Rita Marrano on 14/12/22.
+//
+
+import Foundation
+import SpriteKit
+
+class HowToPlayScene: SKScene {
+    
+    override init(size: CGSize) {
+        
+        super.init(size: size)
+      
+        for index in 0..<2 {
+            let bg = SKSpriteNode(imageNamed: "background")
+            bg.position = CGPoint(x: index * Int(bg.size.width), y: 0)
+            bg.anchorPoint = CGPoint(x: 0, y: 0)
+            bg.name = "background"
+            
+            self.addChild(bg)
+        }
+        
+        
+        //TITLE
+        let label = SKLabelNode(fontNamed: "2Credits")
+        label.text = " WINX CLUB <3 "
+        label.fontSize = 60
+        label.fontColor = .black
+        label.position = CGPoint(x: self.size.width/2 , y: self.size.height/2)
+        self.addChild(label)
+        
+
+        // BACKBUTTON
+        let backButton = SKSpriteNode(imageNamed: "buttonBack")
+        backButton.setScale(2.5)
+        backButton.position = CGPoint(x: self.size.width - 50, y: 330)
+        backButton.name = " backButton "
+        self.addChild(backButton)
+        
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let location = touch.location(in: self)
+            let node = self.nodes(at: location).first
+            
+            if (node?.name == " backButton ") {
+                let reveal : SKTransition = SKTransition.flipHorizontal(withDuration: 0.5)
+                let scene = MenuScene(size: self.view!.bounds.size)
+                scene.scaleMode = .aspectFill
+                self.view?.presentScene(scene, transition:  reveal)
+            }
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init")
+    }
+}
+
